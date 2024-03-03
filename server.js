4be43dcca5a4c6ce71e3ae7a41f377dec8c2a754
MunibaP <munibapervez596@gmail.com>
2024-03-03 17:45:23 -0500

    Added code to the file

---
+++ server.js
@@ -0,0 +1,44 @@
+// Importing required modules and dependencies
+const express = reauire('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Importing express app and Port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Setting Handlebars.js engine with custom helpers
+const hbs = exphbs.create({helpers});
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+// Added session middleware with session object
+app.use(session(sess));
+
+// Import Express.js on the required template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//  Configure Express middleware: parse incoming JSON requests, handle URL-encoded form data, and serve static files from the 'public' directory.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// Sync Sequelize models with the database (if necessary), then start the Express server listening on the specified PORT.
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+});
