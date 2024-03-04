f93affda4b29678f0a81711c372a8d63a91319ec
MunibaP <munibapervez596@gmail.com>
2024-03-04 13:52:14 -0500

    Added code to the file

---
+++ public/js/deletePost.js
@@ -0,0 +1,21 @@
+const deletePost = async (post_id) => {
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'DELETE',
+        headers: {'Content_Type': 'application/json'},
+    });
+    if(response.ok) {
+        document.location.reload();
+    } else {
+        alert('Fail to delete the post');
+    }
+};
+
+const deletePostHandler = (event) => {
+    if(event.target.matches('.delete_post')) {
+        const post_id = event.target.getAttributes('delete_post_id');
+        deletePost(post_id);
+    }
+};
+
+document.addEventListener('click', deletePostHandler);
+
